6a6fbaf870da57d9b3d55e0c631f633299696458
AngelNeves <maryangelafsneves@gmail.com>
2023-10-04 18:13:42 -0300

    finished page, for now

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+
+  {
+    "id":"1",
+    "title": "AGRADECIMENTOS",
+    "description": "  Obrigada por checar o meu projeto, espero que tenha gostado! Apesar da temática divertida, foi muito importante botar em prática as ferramentas do Angular que estou aprendendo no Santander Bootcamp 202 - FullStack Java + Angular, pela DIO. Vocês podem checar meus outros projetos ao clicar no GitHub, localizado no menu acima, ou entrar em contato comigo pelo Linkedin, também localizado acima. Por fim, mas não menos importante, agradeço ao professor Felipe Aguiar por explicar de forma descomplicada todo esse mundo do Desenvolvedor Front-End. Até a próxima :)",
+    "photoCover": "https://scontent.fsdu25-1.fna.fbcdn.net/v/t39.30808-6/321410480_1926238491096282_5559196519512340614_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeEqqm1QOw0Q0O3DCG518KMywoMZqpbIR5zCgxmqlshHnLywBpa9ZDbK5_pxqUmOQ2bZibyRx_f1i2_dqltycmF5&_nc_ohc=5FioXwM8kNMAX_J3b-X&_nc_ht=scontent.fsdu25-1.fna&oh=00_AfDyFQgP3HAtOTocZqT1mgdgya2d5xTrgx11_9qjVolCgA&oe=6522B2FE"
+  },
+
+  {
+    "id":"2",
+    "title": "Recomendações",
+    "description": "Se quiserem um doce bom, eu recomendo tiramisu. Pra quem não gosta de doce muuito doce, ele é perfeito, pois leva café e o café quebra o doce. Mas até mesmo pra você ai que adora um doce docinho, com o creme feito com mascarpone e a calda de chocolate, ele é perfeito. Ou seja, o melhor doce da vida. Obrigada por virem ao meu TedTalk :D ",
+    "photoCover": "https://pbs.twimg.com/media/FBt2fOoX0AAZi8-.jpg:large"
+  }
+]
